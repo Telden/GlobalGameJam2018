1c578a21f567671e0c482000548f153a5652b73f
Evan Janssen <evan.janssen@mymail.champlain.edu>
2018-01-27 19:44:53 -0500

    Hard reset Part 2

---
+++ MarsRover/Assets/Scripts/ComputerPower.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerPower : MonoBehaviour
+{
+	public bool power;
+
+	public GameObject radarCover, camCover, radarLight, screenLight, radarBlink;
+
+	private GameObject rover;
+
+	void Start ()
+	{
+		rover = GameObject.Find("Rover");
+        rover.GetComponent<PlayerMovement>().movementEnabled = false;
+        power = false;
+
+		radarCover.SetActive (true);
+		camCover.SetActive (true);
+		radarLight.SetActive (false);
+		screenLight.SetActive (false);
+
+		radarBlink.SetActive (false);
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.P))
+		{
+			if (power)
+			{
+				power = false;
+
+				radarCover.SetActive (true);
+				camCover.SetActive (true);
+				radarLight.SetActive (false);
+				screenLight.SetActive (false);
+				radarBlink.SetActive (false);
+
+                rover.GetComponent<PlayerMovement>().Respawn();
+                rover.GetComponent<PlayerMovement>().movementEnabled = false;
+            }
+			else
+			{
+
+                rover.GetComponent<PlayerMovement>().movementEnabled = true;
+				power = true;
+
+				radarCover.SetActive (false);
+				camCover.SetActive (false);
+				radarLight.SetActive (true);
+				screenLight.SetActive (true);
+				radarBlink.SetActive (true);
+            }
+		}
+	}
+}
